9fafb2f0c97dd07b4b28cb44eb22030156954dec
friedrich-troescher <friedrich.troescher@maibornwolff.de>
2025-02-05 12:24:43 +0100

    first movable rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
+    float PosX = 0;
+    float PosY = 0;
+
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    window.setFramerateLimit(60);
 
     while (window.isOpen())
     {
@@ -15,7 +19,32 @@ int main()
             }
         }
 
-        window.clear();
+        window.clear(sf::Color::Black);
+        // Draw here
+        // draw a random shape
+
+        sf::RectangleShape rectangle;
+        rectangle.setSize({10, 10});
+        rectangle.setFillColor(sf::Color::White);
+        rectangle.setPosition({PosX, PosY});
+        window.draw(rectangle);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        {
+            PosX++;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+        {
+            PosX--;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+        {
+            PosY--;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
+            PosY++;
+        }
+
         window.display();
     }
 }
