e4d6b619020cfd7807f89b989f80dca83bcb3c21
friedrich-troescher <friedrich.troescher@maibornwolff.de>
2025-02-19 12:22:33 +0100

    clean up main

---
+++ src/include/GameLoop.h
@@ -0,0 +1,15 @@
+//
+// Created by Friedrich Tröscher on 19.02.25.
+//
+
+#ifndef CMAKESFMLPROJECT_GAMELOOP_H
+#define CMAKESFMLPROJECT_GAMELOOP_H
+
+
+class GameLoop {
+public:
+    static void run();
+};
+
+
+#endif //CMAKESFMLPROJECT_GAMELOOP_H
